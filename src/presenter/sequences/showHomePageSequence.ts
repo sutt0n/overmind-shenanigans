60905a8a8fabf5819e583d441fa21826ae31908a
sutt0n <ebwebdev@gmail.com>
2022-02-24 18:35:33 -0600

    wip: implementing favorites loader to home page

--- src/presenter/sequences/showHomePageSequence.ts
+++ src/presenter/sequences/showHomePageSequence.ts
@@ -1,8 +1,11 @@
 import { pipe } from 'overmind';
 import { setPage } from '../operators/setPage';
 import { setIsLoading } from '../operators/setIsLoading';
+import { getFavoriteStarshipsAction } from '../actions/page-home/getFavoriteStarshipsAction';
 
 export const showHomePageSequence = pipe(
   setPage('HOME'),
+  setIsLoading(true),
+  getFavoriteStarshipsAction,
   setIsLoading(false),
 );
